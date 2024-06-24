ab3756bfe1ec9a664978c833585253b029537eb9
Blackpride15 <asu23006@byui.edu>
2024-06-25 00:41:56 +0100

    ,commit

--- scripts/temple.js
+++ scripts/temple.js
@@ -6,6 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.style.display = menu.style.display === "block" ? "none" : "block";
     });
 
-    const lastModifiedSpan = document.getElementById("last-modified");
-    lastModifiedSpan.textContent = document.lastModified;
+    // // Get the current year
+    // document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+    // Get the last modified date of the document
+    const lastModifiedSpan = document.getElementById('last-modified');
+    if (lastModifiedSpan) {
+        const lastModified = new Date(document.lastModified);
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        lastModifiedSpan.textContent = lastModified.toLocaleDateString('en-US', options);
+    }
+    
+
 });
