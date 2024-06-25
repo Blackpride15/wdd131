7fe2783b17774dc3551ac5b4cbb13bdb95b12ba5
Blackpride15 <asu23006@byui.edu>
2024-06-25 14:46:11 +0100

    done

--- scripts/temple.js
+++ scripts/temple.js
@@ -7,12 +7,23 @@ year.innerHTML = `©️ <span id='currentyear'>${today.getFullYear()}</span>`;
 let lastModified = document.lastModified; //define variable
 document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModified}`; //add in html tag
 
-//The function of hamburguer menu
-const menu = document.querySelector('.menu');
-const navigation = document.querySelector('.navigation');
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+const h1 = document.querySelector('h1') //added this for h1 
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    menu.classList.toggle('open');
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+
+function toggleMenu() {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+    h1.classList.toggle('show');
+}
+
+hambutton.addEventListener('click', toggleMenu);
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(min-width: 740px)').matches) {
+        h1.classList.remove('show');
+    }
 
 });
